9d8d2e4227fed62511f4e7afc5bd05568233b903
sunil_manipara <sunil@ecodesoft.com>
2021-02-21 10:26:57 +0530

    push

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -10,6 +10,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   exports: [
@@ -24,6 +25,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatSidenavModule,
     MatSnackBarModule,
     MatToolbarModule,
+    MatTooltipModule,
   ]
 })
 export class MaterialModule { }
